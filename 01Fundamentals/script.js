8a2e3581847529046183c2b134423de72c1c52c0
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-07-07 08:40:47 +0500

    Basic Fundamentals..

---
+++ 01Fundamentals/script.js
@@ -0,0 +1,33 @@
+var arr = [1,2,3,4,"Ahsan"]
+
+
+//foreach, map, filter, find, indexof
+
+// arr.forEach(function(val){
+//     console.log(val)
+// })
+
+
+
+
+// var ans = arr.map(function(val){
+//     return val*2
+// })
+// console.log(ans);
+
+
+
+
+
+var res = arr.find(function(val){
+    if (val == 4) return true;
+    else return false;
+})
+console.log(res);
+
+
+
+
+
+// var res = arr.indexOf(3);
+// console.log(res);
