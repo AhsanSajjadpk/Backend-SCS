5142db41a3af5475acdd11f7dd0410dd999f31c9
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-08-02 00:14:11 +0500

    bcrypt, hash and jwt..

--- 11_Auth_jwt_mongoDB/app.js
+++ 11_Auth_jwt_mongoDB/app.js
@@ -2,16 +2,44 @@ const express = require('express')
 const app = express();
 const cookieParser = require('cookie-parser');
 const path = require('path')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const userModel = require('./models/user')
 
 app.set("view engine", "ejs")
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-app.use(express.static(path.join(__dirname , 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
 
 
-app.get('/', function(req,res){
-    res.send('Hello World!')
+app.get('/', function (req, res) {
+    res.render('index')
+})
+
+
+app.post('/create',  function (req, res) {
+    let { username, email, password, age } = req.body
+
+    bcrypt.genSalt(saltRounds=10, function(err, salt) {
+        bcrypt.hash(password, salt, async function(err, hash) {
+            
+            const createdUser = await userModel.create({
+                username,
+                email,
+                password : hash,
+                age
+            })
+        
+          let token =  jwt.sign({email}, "secretkey")
+          res.cookie("token",token)
+            res.send(createdUser)
+
+        });
+        
+    });
+
+  
 })
 
 app.listen(3000)
