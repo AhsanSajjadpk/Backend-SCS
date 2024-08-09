494508dca4a46f8204587ca6603e740934380de1
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-08-09 13:20:31 +0500

    login/Logout user Done with protected route..

--- 13_Project_Data_Association/app.js
+++ 13_Project_Data_Association/app.js
@@ -18,7 +18,18 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
+app.get('/profile', isLoggedIn, (req, res) => {
+    console.log(req.user)
+    res.render('profile')
+})
+app.get('/logout', (req, res) => {
+    res.cookie("token", "")
+    res.redirect("/")
+})
 
 
 app.post('/register', async (req, res) => {
@@ -34,17 +45,47 @@ app.post('/register', async (req, res) => {
                 name, username, age, email,
                 password: hash
             })
-            let token = jwt.sign({email : email , userid : user._id}, "secretekry")
-            res.cookie("token", token) 
+            let token = jwt.sign({ email: email, userid: user._id }, "secretekry")
+            res.cookie("token", token)
             res.send("User Created .")
         })
     })
 })
 
 
+app.post('/login', async (req, res) => {
+
+    let { email, password } = req.body
+    let user = await userModel.findOne({ email })
+    if (!user) return res.status(500).send("Something went wrong")
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+            let token = jwt.sign({ email: email, userid: user._id }, "secretekry")
+            res.cookie("token", token)
+            res.send("you can login")
+        }
+        else {
+            res.redirect("/login")
+        }
+    })
+})
 
 
 
+function isLoggedIn(req, res, next) {
+
+    if (req.cookies.token === "") {
+        console.log(req.cookies)
+        res.send("You must be logged in")
+    }
+    else {
+        let data = jwt.verify(req.cookies.token, "secretekry") // data ma email aur userid ayaga jo ham na jwt ma signn kea tha
+        req.user = data
+        next()
+    }
+
+}
 
 
 
